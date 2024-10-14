7b2c244364908600ab870329d87c01e2b3066351
LDiego2800 <Luis.mezarina2800@gmail.com>
2024-10-13 21:05:15 -0500

    version para enviar

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const preguntaNombre = prompt("¿Cual es tu nombre?");
 let preguntaGustos = prompt("¿Te gusta la programación?");
 const colorFondo = prompt("Elige de que color quieres que se muestre esta pagina");
 
+alert(preguntaNombre)
+alert(preguntaGustos)
+alert(colorFondo)
+
 
 
 const preguntaCorreo = prompt("Si quieres recibir más infomación indicanos tu correo");
