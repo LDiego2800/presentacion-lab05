dcccbce816136049aab7fdace2085c31e37c1a8f
LDiego2800 <Luis.mezarina2800@gmail.com>
2024-10-13 20:00:16 -0500

    primer avance

---
+++ app.js
@@ -0,0 +1,7 @@
+const preguntaNombre = prompt("¿Cual es tu nombre?");
+let preguntaGustos = prompt("¿Te gusta la programación?");
+const colorFondo = prompt("Elige de que color quieres que se muestre esta pagina");
+
+
+
+const preguntaCorreo = prompt("Si quieres recibir más infomación indicanos tu correo");
